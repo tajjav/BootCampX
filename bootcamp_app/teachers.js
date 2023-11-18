ad75224edef44a027067def8f9cbf6d7336655fc
tajjav <taj.jav@hotmail.com>
2023-11-18 12:12:53 -0700

    add SQL injection prevention implementation for students.js and teachers.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,29 +6,62 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+// preventing SQL injection
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1 
+  ORDER BY teacher;
+`;
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+pool.query(queryString,values)
+    .then(res => {
+      // console.log(res.rows);
+      res.rows.forEach(user => {
+        console.log(`
+        
+        ${user.teacher} has assisted the ${user.cohort} cohort.
+
+        `);
+      })
+    })
+    .catch(err => {
+    console.log('query error: ',err.stack);
+    });
 
 
-pool.query(`
 
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
 
-`)
-.then(res => {
-  // console.log(res.rows);
-  res.rows.forEach(user => {
-    console.log(`
-    
-    ${user.teacher} has assisted the ${user.cohort} cohort.
 
-    `);
-  })
-})
-.catch(err => {
-console.log('query error: ',err.stack);
-});
+
+
+
+
+// without SQL injection attack prevention
+// pool.query(`
+//   SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+//   FROM teachers
+//   JOIN assistance_requests ON teacher_id = teachers.id
+//   JOIN students ON student_id = students.id
+//   JOIN cohorts ON cohort_id = cohorts.id
+//   WHERE cohorts.name LIKE '%${process.argv[2]}%'
+//   ORDER BY teacher;
+//   `)
+//   .then(res => {
+//     // console.log(res.rows);
+//     res.rows.forEach(user => {
+//       console.log(`
+      
+//       ${user.teacher} has assisted the ${user.cohort} cohort.
+
+//       `);
+//     })
+//   })
+//   .catch(err => {
+//   console.log('query error: ',err.stack);
+//   });
